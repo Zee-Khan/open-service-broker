aa761eb1c6add7ab981e957bc42c411895cc05e2
Zohaib Khan <zohaib.khan@swisscom.com>
2018-03-26 16:20:01 +0200

    Unable to simplifiy release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,20 +2,20 @@
 
 cd `dirname $0`/..
 
-if [ "$#" -lt 1 ]; then
-    echo "Usage: $(basename $0) osb_release_version [branch_to_release_from/develop] [branch_to_push_to/master]"
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $(basename $0) osb_release_version osb_next_dev_version [branch_to_release_from/develop] [branch_to_push_to/master]"
     exit 1
 fi
 
 branch_to_release_from=develop
 branch_to_push_to=master
 
-if [ "$#" -ge 2 ]; then
-    branch_to_release_from=$2
+if [ "$#" -ge 3 ]; then
+    branch_to_release_from=$3
 fi
 
-if [ "$#" -ge 3 ]; then
-    branch_to_push_to=$3
+if [ "$#" -ge 4 ]; then
+    branch_to_push_to=$4
 fi
 
 if [[ -n $(git status -s) ]]; then
@@ -55,7 +55,7 @@ git push origin $branch_to_push_to --tags
 git checkout $branch_to_release_from
 git merge releases/$1 --no-ff -m "Merge branch 'releases/$1' into $branch_to_release_from"
 git branch -d releases/$1
-./scripts/set-version.sh "$1-SNAPSHOT"
+./scripts/set-version.sh $2
 git commit --no-verify -am "Bump next $branch_to_release_from version"
 git --no-pager diff origin/$branch_to_release_from
 git push origin $branch_to_release_from
@@ -66,4 +66,4 @@ echo releases/$1 has been merged into $branch_to_push_to, tagged and pushed
 echo
 echo releases/$1 has been merged into $branch_to_release_from
 echo
-echo OSB version bumped to "$1-SNAPSHOT" on $branch_to_release_from
+echo OSB version bumped to $2 on $branch_to_release_from
